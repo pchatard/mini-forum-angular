1d8aed1e1fe5bfee40e0256c3283a913f585b4c4
Fatih <yildizfatih96@gmail.com>
2021-06-21 13:59:06 +0200

    Displays are created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TopicComponent } from './pages/topic/topic.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UpdateUserComponent } from './pages/update-user/update-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    TopicComponent,
+    RegisterComponent,
+    UpdateUserComponent
   ],
   imports: [
     BrowserModule,
