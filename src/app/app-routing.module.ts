b82a65c280d6afabe1a4d9afe1d81c8dc6e757ed
Fatih <yildizfatih96@gmail.com>
2021-06-21 14:18:32 +0200

    Router cofiguration is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { TopicComponent } from './components/pages/topic/topic.component';
+import { UpdateUserComponent } from './components/pages/update-user/update-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'home', component: HomeComponent},
+{ path: 'login', component: LoginComponent},
+{ path: 'register', component: RegisterComponent},
+{ path: 'topic', component: TopicComponent},
+{ path: 'update-user', component: UpdateUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
