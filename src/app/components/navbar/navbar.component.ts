67d355f09d9bfec5491e257e328fb9046aae5e78
Blayn <jad.sebahi@gmail.com>
2021-06-22 14:42:59 +0200

    Navbar display changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -6,11 +7,24 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
+
+  isUserConnected:boolean=false;
+  userSubscription!:Subscription;
 
   constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.userSubscription=this.auth.userSubject.subscribe(user=>{
+      if(user)
+      {
+        this.isUserConnected=true;
+      } else
+      {
+        this.isUserConnected=false;
+      }
+    });
   }
 
   logout(): void {
