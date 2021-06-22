5ebfb3e62c67abb283330fe805e48478ea31ad7f
Pierre Chatard <chatard.pierre@gmail.com>
2021-06-22 10:50:53 +0200

    Create update and delete methods for user

--- src/app/components/pages/update-user/update-user.component.ts
+++ src/app/components/pages/update-user/update-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-update-user',
@@ -10,19 +12,28 @@ export class UpdateUserComponent implements OnInit {
 
   updateUserForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private auth: AuthService, private formBuilder: FormBuilder, private userService: UsersService) { }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  onSubmit(): void {
+    this.userService.modifyUser({ ...this.updateUserForm.value, id: this.auth.user.id });
+    this.buildForm();
+  }
+
+  deleteAccount(): void {
+    this.userService.deleteUser(this.auth.user.id);
+  }
+
+  buildForm(): void {
     this.updateUserForm = this.formBuilder.group({
-      username: ['', [Validators.required]],
+      username: [this.auth.user.username, [Validators.required]],
       password: ['', [Validators.required]],
       passwordConfirm: ['', [Validators.required]],
       oldPassword: ['', [Validators.required]]
     });
-  }
-
-  onSubmit(): void {
-    console.log(this.updateUserForm.value);
   }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -10,11 +10,27 @@ export class UsersService {
 
   constructor(private http: HttpClient, private auth: AuthService) { }
 
-  register(newUser: any): any {
+  register(newUser: any): void {
     this.http.post(`${environment.apiUrl}api/user`, newUser).subscribe(user => {
       this.auth.login({ username: newUser.username, password: newUser.password });
     }, error => {
       console.log(error);
     });
+  }
+
+  modifyUser(user: any): void {
+    this.http.patch(`${environment.apiUrl}api/user/${user.id}`, { username: user.username, password: user.password, passwordConfirm: user.passwordConfirm, oldPassword: user.oldPassword }).subscribe(updatedUser => {
+      console.log(updatedUser);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  deleteUser(userId: number): void {
+    this.http.delete(`${environment.apiUrl}api/user/${userId}`).subscribe(() => {
+      this.auth.logout();
+    }, error => {
+      console.log(error);
+    })
   }
 }
